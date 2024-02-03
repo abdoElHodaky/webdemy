385f397522014ee42fd5eb8af0ecbc5da94a7040
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-02-03 10:26:41 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,7 +65,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
+            'url' => env('DATABASE_URL',"postgres://xuhjpnur:XwL4qz4lznD4rC47XXNbfHKAFZONnOD3@arjuna.db.elephantsql.com/xuhjpnur"),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
